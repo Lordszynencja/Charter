65cac2bd725cdc09a309d6f6ed8c11caa1cd30ab
Tamamo <lordszynencja@gmail.com>
2023-07-20 22:10:52 +0200

    updated version

--- src/main/java/log/charter/main/LogCharterRSMain.java
+++ src/main/java/log/charter/main/LogCharterRSMain.java
@@ -7,7 +7,7 @@ import log.charter.data.config.Localization.Label;
 import log.charter.gui.CharterFrame;
 
 public class LogCharterRSMain {
-	public static final String VERSION = "0.9.11 - 2023.07.19";
+	public static final String VERSION = "0.9.12 - 2023.07.20";
 	public static final String TITLE = "LoG Charter RS";
 
 	public static void main(final String[] args) throws InterruptedException, IOException {
