8c6c7d38fce8131e8434e182339852af88af52c3
Tamamo <lordszynencja@gmail.com>
2024-02-18 19:59:08 +0100

    version number change

--- src/main/java/log/charter/main/LogCharterRSMain.java
+++ src/main/java/log/charter/main/LogCharterRSMain.java
@@ -9,7 +9,7 @@ import log.charter.gui.CharterFrame;
 import log.charter.io.Logger;
 
 public class LogCharterRSMain {
-	public static final String VERSION = "0.14.2";
+	public static final String VERSION = "0.15.0";
 	public static final String VERSION_DATE = "2024.02.18";
 	public static final String TITLE = "LoG Charter RS " + VERSION;
 
