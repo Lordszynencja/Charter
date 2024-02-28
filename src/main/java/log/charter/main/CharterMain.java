7151471b13a19e1013b34076e40d4b0c296db3e1
Tamamo <lordszynencja@gmail.com>
2024-02-28 01:04:09 +0100

    changed version

--- src/main/java/log/charter/main/CharterMain.java
+++ src/main/java/log/charter/main/CharterMain.java
@@ -9,8 +9,8 @@ import log.charter.gui.CharterFrame;
 import log.charter.io.Logger;
 
 public class CharterMain {
-	public static final String VERSION = "0.15.2";
-	public static final String VERSION_DATE = "2024.02.27";
+	public static final String VERSION = "0.15.3";
+	public static final String VERSION_DATE = "2024.02.28";
 	public static final String TITLE = "Charter " + VERSION;
 
 	public static void main(final String[] args) throws InterruptedException, IOException {
