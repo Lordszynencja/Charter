81d2eadafe995a12ab8b2202d8f76026a2e8dab7
Lordszynencja <lordszynencja@gmail.com>
2024-10-15 16:44:51 +0200

    added a donation button (#44)

--- src/main/java/log/charter/CharterMain.java
+++ src/main/java/log/charter/CharterMain.java
@@ -9,8 +9,8 @@ import log.charter.services.CharterContext;
 import log.charter.services.mouseAndKeyboard.ShortcutConfig;
 
 public class CharterMain {
-	public static final String VERSION = "0.17.8";
-	public static final String VERSION_DATE = "2024.10.15 13:11";
+	public static final String VERSION = "0.17.9";
+	public static final String VERSION_DATE = "2024.10.15 14:43";
 	public static final String TITLE = "Charter " + VERSION;
 
 	public static void main(final String[] args) throws InterruptedException, IOException {
