6ff2b648d88fd63fb5f443a7eb80da71a315cc30
Lordszynencja <lordszynencja@gmail.com>
2025-01-13 00:59:37 +0100

    0.17.14 (#52)
    * changed error for wrong music file
    
    * changed version

--- src/main/java/log/charter/CharterMain.java
+++ src/main/java/log/charter/CharterMain.java
@@ -9,8 +9,8 @@ import log.charter.services.CharterContext;
 import log.charter.services.mouseAndKeyboard.ShortcutConfig;
 
 public class CharterMain {
-	public static final String VERSION = "0.17.13";
-	public static final String VERSION_DATE = "2025.01.04 21:05";
+	public static final String VERSION = "0.17.14";
+	public static final String VERSION_DATE = "2025.01.12 23:53";
 	public static final String TITLE = "Charter " + VERSION;
 
 	public static void main(final String[] args) throws InterruptedException, IOException {
