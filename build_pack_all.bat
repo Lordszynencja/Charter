226e6b3cfdc46ee7db8a44d9c2b404162c48e375
Lordszynencja <lordszynencja@gmail.com>
2025-02-01 00:07:27 +0100

    made showing FFt only with debug option (#64)

--- build_pack_all.bat
+++ build_pack_all.bat
@@ -2,5 +2,5 @@
 call "set version.bat"
 call "build_pack_windows.bat"
 call "build_pack_linux.bat"
-call "build_pack_mac.bat"
-call "build_pack_mac_arm.bat"
+::call "build_pack_mac.bat"
+::call "build_pack_mac_arm.bat"
