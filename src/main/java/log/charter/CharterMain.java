3c44ff5f7dc0d74f3ab319903fe9ea95b2fafa2a
Tamamo <lordszynencja@gmail.com>
2025-02-24 22:25:19 +0100

    updated version string

--- src/main/java/log/charter/CharterMain.java
+++ src/main/java/log/charter/CharterMain.java
@@ -10,7 +10,7 @@ import log.charter.services.mouseAndKeyboard.ShortcutConfig;
 
 public class CharterMain {
 	public static final String VERSION = "0.20.0";
-	public static final String VERSION_DATE = "2025.02.22 12:00";
+	public static final String VERSION_DATE = "2025.02.24 21:30";
 	public static final String TITLE = "Charter " + VERSION;
 
 	public static void main(final String[] args) throws InterruptedException, IOException {
