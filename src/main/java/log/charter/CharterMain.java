3760ac71cff37d60ec61f8402477693c05ada3d0
Tamamo <lordszynencja@gmail.com>
2026-01-28 17:05:14 +0100

    changed versions

--- src/main/java/log/charter/CharterMain.java
+++ src/main/java/log/charter/CharterMain.java
@@ -12,8 +12,8 @@ import log.charter.services.mouseAndKeyboard.ShortcutConfig;
 import log.charter.util.RW;
 
 public class CharterMain {
-	public static final String VERSION = "0.21.16";
-	public static final String VERSION_DATE = "2026.01.28 02:25";
+	public static final String VERSION = "0.21.17";
+	public static final String VERSION_DATE = "2026.01.28 15:05";
 	public static final String TITLE = "Charter " + VERSION;
 
 	private static void deleteTempUpdateFile() {
