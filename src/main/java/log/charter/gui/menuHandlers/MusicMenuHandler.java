421a6326b6995c6144cc2ba06f4f3a1d119eb817
Tamamo <lordszynencja@gmail.com>
2026-02-04 15:20:30 +0100

    added stem settings menu option

--- src/main/java/log/charter/gui/menuHandlers/MusicMenuHandler.java
+++ src/main/java/log/charter/gui/menuHandlers/MusicMenuHandler.java
@@ -2,13 +2,20 @@ package log.charter.gui.menuHandlers;
 
 import javax.swing.JMenu;
 
+import log.charter.data.ChartData;
 import log.charter.data.config.Localization.Label;
+import log.charter.gui.components.toolbar.AudioStemsSettings;
+import log.charter.gui.components.toolbar.ChartToolbar;
 import log.charter.services.Action;
+import log.charter.services.data.ProjectAudioHandler;
 import log.charter.services.editModes.EditMode;
 import log.charter.services.editModes.ModeManager;
 
 class MusicMenuHandler extends CharterMenuHandler {
+	private ChartData chartData;
+	private ChartToolbar chartToolbar;
 	private ModeManager modeManager;
+	private ProjectAudioHandler projectAudioHandler;
 
 	@Override
 	boolean isApplicable() {
@@ -29,6 +36,13 @@ class MusicMenuHandler extends CharterMenuHandler {
 		menu.add(createItem(Action.TOGGLE_REPEAT_START));
 		menu.add(createItem(Action.TOGGLE_REPEAT_END));
 
+		menu.addSeparator();
+		menu.add(createItem(Label.AUDIO_STEM_SETTINGS, this::openStemSettings));
+
 		return menu;
+	}
+
+	private void openStemSettings() {
+		new AudioStemsSettings(chartData, charterFrame, chartToolbar, projectAudioHandler);
 	}
 }
