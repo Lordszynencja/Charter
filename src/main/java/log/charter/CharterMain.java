3c0351d6e2a1b24b41b88d7f26eefd9a8fa2342e
Lordszynencja <lordszynencja@gmail.com>
2026-02-11 03:13:29 +0100

    V0.21.25 (#148)
    * fixed jumping to next/previous beat being off
    
    * added automatic t-> T for fingering
    
    * fixed wrong distance calculation on notes close to beat start
    
    * updated version

--- src/main/java/log/charter/CharterMain.java
+++ src/main/java/log/charter/CharterMain.java
@@ -12,8 +12,8 @@ import log.charter.services.mouseAndKeyboard.ShortcutConfig;
 import log.charter.util.RW;
 
 public class CharterMain {
-	public static final String VERSION = "0.21.24";
-	public static final String VERSION_DATE = "2026.02.10 15:00";
+	public static final String VERSION = "0.21.25";
+	public static final String VERSION_DATE = "2026.02.11 01:11";
 	public static final String TITLE = "Charter " + VERSION;
 
 	private static void deleteTempUpdateFile() {
