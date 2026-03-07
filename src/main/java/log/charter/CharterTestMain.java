dba90c4a41db4644f59651eabfef8c2a80445af6
Lordszynencja <lordszynencja@gmail.com>
2026-03-08 00:36:35 +0100

    added javafx window test

---
+++ src/main/java/log/charter/CharterTestMain.java
@@ -0,0 +1,49 @@
+package log.charter;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import log.charter.util.collections.ExpiringValuesList;
+
+public class CharterTestMain extends Application {
+	private static final ExpiringValuesList<Long> framesCounter = new ExpiringValuesList<>(ExpiringValuesList.second);
+
+	public static void main(final String[] args) {
+		launch();
+	}
+
+	private static final String javaVersion = System.getProperty("java.version");
+	private static final String javafxVersion = System.getProperty("javafx.version");
+	private final Label label;
+
+	{
+		label = new Label("");
+		showFPS();
+	}
+
+	private void showFPS() {
+		label.setText(
+				"JavaFX: " + javafxVersion + ", Java: " + javaVersion + ", FPS: " + framesCounter.getValues().size());
+	}
+
+	@Override
+	public void start(final Stage stage) {
+		final Scene scene = new Scene(new StackPane(label), 640, 480);
+		stage.setScene(scene);
+		stage.show();
+
+		final AnimationTimer frameRateMeter = new AnimationTimer() {
+
+			@Override
+			public void handle(final long now) {
+				framesCounter.addValue(now);
+				showFPS();
+			}
+		};
+
+		frameRateMeter.start();
+	}
+}
