ed5265cfde83ba2a49c22137fada1c295efe0136
Lordszynencja <lordszynencja@gmail.com>
2026-03-09 12:55:53 +0100

    changed back lwjgl library versions (#176)

--- src/main/java/log/charter/CharterMain.java
+++ src/main/java/log/charter/CharterMain.java
@@ -12,8 +12,8 @@ import log.charter.services.mouseAndKeyboard.ShortcutConfig;
 import log.charter.util.RW;
 
 public class CharterMain {
-	public static final String VERSION = "0.22.11";
-	public static final String VERSION_DATE = "2026.03.08 00:00";
+	public static final String VERSION = "0.22.12";
+	public static final String VERSION_DATE = "2026.03.09 12:00";
 	public static final String TITLE = "Charter " + VERSION;
 
 	private static void deleteTempUpdateFile() {
