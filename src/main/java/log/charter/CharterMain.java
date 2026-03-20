e1739045542bb7f5dd9539a6a441bd1fea441fd1
Lordszynencja <lordszynencja@gmail.com>
2026-03-20 02:11:54 +0100

    added slap/pop shortcut to config list

--- src/main/java/log/charter/CharterMain.java
+++ src/main/java/log/charter/CharterMain.java
@@ -12,8 +12,8 @@ import log.charter.services.mouseAndKeyboard.ShortcutConfig;
 import log.charter.util.RW;
 
 public class CharterMain {
-	public static final String VERSION = "0.22.14";
-	public static final String VERSION_DATE = "2026.03.12 02:00";
+	public static final String VERSION = "0.22.15";
+	public static final String VERSION_DATE = "2026.03.20 01:00";
 	public static final String TITLE = "Charter " + VERSION;
 
 	private static void deleteTempUpdateFile() {
