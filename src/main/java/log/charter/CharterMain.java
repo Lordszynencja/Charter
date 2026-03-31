cbe99d438723154b2f0e578fd7e24e8dfc51b206
Lordszynencja <lordszynencja@gmail.com>
2026-04-01 00:58:41 +0200

    removed manual

--- src/main/java/log/charter/CharterMain.java
+++ src/main/java/log/charter/CharterMain.java
@@ -13,7 +13,7 @@ import log.charter.util.RW;
 
 public class CharterMain {
 	public static final String VERSION = "0.22.19";
-	public static final String VERSION_DATE = "2026.03.30 17:00";
+	public static final String VERSION_DATE = "2026.04.01 00:00";
 	public static final String TITLE = "Charter " + VERSION;
 
 	private static void deleteTempUpdateFile() {
