d444d894a88eba11f754853382117e79f4b976a8
Lordszynencja <lordszynencja@gmail.com>
2026-09-18 04:00:24 +0200

    updated version

--- src/main/java/log/charter/CharterMain.java
+++ src/main/java/log/charter/CharterMain.java
@@ -13,7 +13,7 @@ import log.charter.util.RW;
 
 public class CharterMain {
 	public static final String VERSION = "0.22.41";
-	public static final String VERSION_DATE = "2026.09.17 17:00";
+	public static final String VERSION_DATE = "2026.09.18 02:00";
 	public static final String TITLE = "Charter " + VERSION;
 
 	private static void deleteTempUpdateFile() {
